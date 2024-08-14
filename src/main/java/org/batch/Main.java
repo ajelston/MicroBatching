76ba7c55ee133b51d084e4c7c334fc52e6869849
Adam Elston <adam@elaboratus.org>
2024-08-14 17:08:11 +1000

    Demo application in Main

--- src/main/java/org/batch/Main.java
+++ src/main/java/org/batch/Main.java
@@ -1,7 +1,15 @@
 package org.batch;
 
-import java.util.List;
+import org.batch.demo.PlusOneBatchProcessor;
+import org.batch.impl.MBBatcher;
+import org.batch.impl.MBBatcherOptions;
+import org.batch.impl.MBJob;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.*;
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
@@ -9,7 +17,71 @@ public class Main {
     public static void main(String[] args) {
         LOGGER.info("MicroBatcher demo starting.");
 
+        int numCallerThreads = 10;
+        int numJobsPerThread = 1_000_000;
+
+        // TODO: arguments for options/defaults, num threads
+        // TODO: Refactor - create Callable/Runnable objects 
+        MBBatcherOptions options = new MBBatcherOptions.Builder()
+                .withBatchSize(5)
+                .withTimeout(Duration.ofMillis(100))
+                .build();
+
+        BatchProcessor<Integer, Integer> batchProcessor = new PlusOneBatchProcessor();
+        MicroBatcher<Integer, Integer> microBatcher = new MBBatcher<>(options, batchProcessor);
+
+        Instant start = Instant.now();
+        try (ExecutorService executorService = Executors.newFixedThreadPool(numCallerThreads)) {
+            IntStream.range(0, numCallerThreads)
+                    .mapToObj(threadNum -> (Callable<Integer>) () -> {
+                        Instant threadStart = Instant.now();
+                        var jobFutures = IntStream.range(0, numJobsPerThread)
+                                .mapToObj(jobNum -> {
+                                    var job = new MBJob<Integer>();
+                                    job.setInput(jobNum);
+                                    LOGGER.fine(String.format("Thread %d submitting job %d", threadNum, jobNum));
+                                    return microBatcher.submit(job);
+                                });
+                        Instant submitEnd = Instant.now();
+                        var submitDuration = Duration.between(threadStart, submitEnd);
+                        reportDuration(
+                                String.format("Thread %d jobs submitted", threadNum),
+                                submitDuration,
+                                numJobsPerThread);
+                        jobFutures.forEach(future ->
+                                future.thenAccept(jobResult -> {
+                                    var message = String.format("Thread %d job %d completed", threadNum, jobResult.getResult());
+                                    LOGGER.fine(message);
+                                }));
+
+                        return threadNum;
+                    }).map(executorService::submit)
+                    .forEach(future -> {
+                        try {
+                            var threadNum = future.get();
+                            LOGGER.info(String.format("Thread %d completed", threadNum));
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }
+
+        LOGGER.info("Shutting down MicroBatcher and waiting for submitted jobs to process.");
+        microBatcher.shutdown();
+
+        Instant end = Instant.now();
+        reportDuration(
+                "All jobs submitted and processed",
+                Duration.between(start, end),
+                numCallerThreads * numJobsPerThread);
 
         LOGGER.info("MicroBatcher demo completed.");
+    }
+
+    private static void reportDuration(String description, Duration elapsed, int numItems) {
+        var message = String.format("%s: %d milliseconds, %d items, %f nanoseconds/item",
+                description, elapsed.toMillis(), numItems,
+                ((double) elapsed.toNanos()) / numItems);
+        LOGGER.info(message);
     }
 }
---
+++ src/main/java/org/batch/demo/PlusOneBatchProcessor.java
@@ -0,0 +1,24 @@
+package org.batch.demo;
+
+import org.batch.BatchProcessor;
+import org.batch.Job;
+import org.batch.JobResult;
+import org.batch.impl.MBJobResult;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A demonstration BatchProcessor that adds one to Job input.
+ */
+public class PlusOneBatchProcessor implements BatchProcessor<Integer, Integer> {
+    @Override
+    public List<JobResult<Integer>> process(List<Job<Integer>> jobs) {
+        return jobs.stream()
+                .map(job -> {
+                    MBJobResult<Integer> result = new MBJobResult<>();
+                    result.setResult(job.getInput() + 1);
+                    return result;
+                }).collect(Collectors.toList());
+    }
+}
